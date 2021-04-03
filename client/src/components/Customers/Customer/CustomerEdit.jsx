8c8a141273578eb8660f609e18282aed0807cbf7
Rogelio Rios <rogeliorios348@gmail.com>
2021-04-03 17:41:08 -0500

    Customer Edit Page - Added responsive to content with in page - Basic layer done

--- client/src/components/Customers/Customer/CustomerEdit.jsx
+++ client/src/components/Customers/Customer/CustomerEdit.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import { Box, Flex , Text, Editable, Badge, Button, Grid } from "@chakra-ui/react";
+import { Box, Flex , Text, Editable, EditablePreview, EditableInput, EditableControls, Badge, Button, Grid } from "@chakra-ui/react";
 import {Link} from 'react-router-dom';
 
+
 const CustomerEdit = () => {
+    
+
+
     return (
-        <Flex direction='column'>
-            <Box pt='2rem' pb='1rem' >
+        <Flex direction='column' justifyContent='center' pb='2rem' pt='2rem' w={[300, 400, 800]}>
+            <Box pt='2rem' pb='1rem'>
                 <Box>
                     <Link to='/customers'>
                         <Text>Go Back</Text>
                     </Link>
                 </Box>
             </Box>
-            <Box display='flex' pt='2rem'>
-                <Box display='flex' p='1rem' bg='gray.700' rounded='2xl' shadow='md'>
+            <Box display='flex' pt='1rem' justifyContent='center'>
+                <Box display='flex' p='1rem' bg='gray.700' rounded='2xl' shadow='md' w={[300, 400, 800]}>
                     <Box display='flex' mr='auto'>
-                        <Box display='flex' pr='1rem'>
+                        <Box display='flex' flexDir='column' justifyContent='center' pr='1rem'>
                             <Text>Status</Text>
                         </Box>
                         <Box >
@@ -32,11 +36,12 @@ const CustomerEdit = () => {
                     </Box>
                 </Box>
             </Box>
-            <Box display='flex' pt='2rem' >
-                <Box display='flex' flexDir='column' p='1rem' bg='gray.700' rounded='2xl' shadow='md'>
-                    <Box display='flex' >
+            <Box display='flex' pt='2rem'  justifyContent='center'>
+                <Box display='flex' flexDir='column' p='1rem' bg='gray.700' rounded='2xl' shadow='md' w={[300, 400, 800]}>
+                    <Box display='flex' p='2rem'>
                         <Box>
-                            Customer ID
+
+                            <Text fontSize='40px' fontWeight='bold'>#1</Text>
                         </Box>
                         <Box display='flex' flexDir='column' ml='auto'>
                             <Text>150 Tallant St</Text>
@@ -45,7 +50,7 @@ const CustomerEdit = () => {
                         </Box>
                     </Box>
                     <Box display='flex' >
-                        <Box display='flex' flexDir='column'>
+                        <Box display='flex' flexDir='column' p='1rem'>
                             <Box>
                                 Customer name 
                             </Box>
@@ -53,12 +58,20 @@ const CustomerEdit = () => {
                                 Customer phone
                             </Box>
                         </Box>
-                        <Box display='flex' flexDir='column'>
-                            Address
-                            City
-                            Country 
+                        <Box display='flex' flexDir='column' p='1rem'>
+                            <Box>
+                                Address 
+                            </Box>
+                            <Box>
+                                City
+                            </Box>
+                            <Box>
+                                Country
+                            </Box>
+                            
                         </Box>
-                        <Box display='flex' flexDir='column'>
+                        <Box display='flex' flexDir='column' p='1rem'>
+
                             Customer email
                         </Box>
                     </Box>
