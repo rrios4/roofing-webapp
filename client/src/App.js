9d2fb10826faa1b21243a64e5aa9a5e1c1baa17a
Rogelio Rios <rogeliorios348@gmail.com>
2022-07-01 04:14:21 -0500

    Moved chakra ui to index from app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import {ChakraProvider, Flex, ThemeProvider, theme, ColorModeProvider, CSSReset} from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react";
 import { Customers, Navbar, Invoices, Estimates, Employees, DashBoard, EstimateRequests  } from "./components/";
 import CustomerEdit from './components/Customers/Customer/CustomerEdit'
 import InvoiceEdit from './components/Invoices/Invoice/InvoiceEdit'
@@ -12,34 +12,25 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
     return (
-            <BrowserRouter>
-                    <ThemeProvider theme={theme}>
-                        <ColorModeProvider options={{
-                            useSystemColorMode: false
-                        }}>
-                        <CSSReset />
-                        <ChakraProvider>
-                            <Navbar/>
-                            <Flex ml={{base:'0rem', lg:'8rem'}} mt={{base:'4rem', lg:'0rem'}} justifyContent='center'>
-                            <Routes>
-                                    <Route path="/" element={<ProtectedRoute><DashBoard/></ProtectedRoute>}/>
-                                    <Route path="/login" element={<Login/>}/>
-                                    <Route path="/signup" element={<SignUp/>}/>
-                                    <Route path='/estimate-requests' element={<ProtectedRoute><EstimateRequests/></ProtectedRoute>}/>
-                                    <Route path='/customers' element={<ProtectedRoute><Customers/></ProtectedRoute>}/>
-                                    <Route path='/invoices' element={<ProtectedRoute><Invoices/></ProtectedRoute>}/>
-                                    <Route path='/estimates' element={<ProtectedRoute><Estimates/></ProtectedRoute>}/>
-                                    <Route path='/employees' element={<ProtectedRoute><Employees/></ProtectedRoute>}/>
-                                    <Route path='/editcustomer/:id' element={<ProtectedRoute><CustomerEdit/></ProtectedRoute>}/>
-                                    <Route path='/editinvoice/:id' element={<ProtectedRoute><InvoiceEdit/></ProtectedRoute>}/>
-                                    <Route path="/editestimate/:id" element={<ProtectedRoute><EstimateEdit/></ProtectedRoute>}/>
-                                    <Route path="/editemployee/:id" element={<ProtectedRoute><EmployeeEdit/></ProtectedRoute>}/>
-                            </Routes>
-                            </Flex>
-                        </ChakraProvider>
-                        </ColorModeProvider>
-                    </ThemeProvider>
-            </BrowserRouter>
+        <BrowserRouter>
+            <Navbar/>
+            <Flex ml={{base:'0rem', lg:'8rem'}} mt={{base:'4rem', lg:'0rem'}} justifyContent='center'>
+            <Routes>
+                    <Route path="/" element={<ProtectedRoute><DashBoard/></ProtectedRoute>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/signup" element={<SignUp/>}/>
+                    <Route path='/estimate-requests' element={<ProtectedRoute><EstimateRequests/></ProtectedRoute>}/>
+                    <Route path='/customers' element={<ProtectedRoute><Customers/></ProtectedRoute>}/>
+                    <Route path='/invoices' element={<ProtectedRoute><Invoices/></ProtectedRoute>}/>
+                    <Route path='/estimates' element={<ProtectedRoute><Estimates/></ProtectedRoute>}/>
+                    <Route path='/employees' element={<ProtectedRoute><Employees/></ProtectedRoute>}/>
+                    <Route path='/editcustomer/:id' element={<ProtectedRoute><CustomerEdit/></ProtectedRoute>}/>
+                    <Route path='/editinvoice/:id' element={<ProtectedRoute><InvoiceEdit/></ProtectedRoute>}/>
+                    <Route path="/editestimate/:id" element={<ProtectedRoute><EstimateEdit/></ProtectedRoute>}/>
+                    <Route path="/editemployee/:id" element={<ProtectedRoute><EmployeeEdit/></ProtectedRoute>}/>
+            </Routes>
+            </Flex>
+        </BrowserRouter>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AuthProvider } from './components/Authentication/auth';
+import { ColorModeScript, ThemeProvider, ChakraProvider, CSSReset } from '@chakra-ui/react';
+import theme from './theme'
 
 import App from './App';
 
-ReactDOM.render(<AuthProvider><App /></AuthProvider>, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <AuthProvider>
+            <ThemeProvider theme={theme}>
+                <CSSReset />
+                <ChakraProvider>
+                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+                <App />
+                </ChakraProvider>
+            </ThemeProvider>
+        </AuthProvider>
+    </>
+, document.getElementById('root'));
