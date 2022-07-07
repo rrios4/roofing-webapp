80e14cd178c89c55da2dbd163d8dbe8accf6692f
Rogelio Rios <rogeliorios348@gmail.com>
2022-07-07 15:39:08 -0500

    Dynamic Dark/Light Mode Toggle - Updated toggle to change depending on colorMode of app

--- client/src/components/Navbar/Toggle.js
+++ client/src/components/Navbar/Toggle.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Flex, IconButton, Tooltip, useColorMode } from "@chakra-ui/react";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 export default function Toggle() {
     const {colorMode, toggleColorMode } = useColorMode();
     return (
         <div>
             <Flex align='center' justify='center' height='4rem' direction='column'>
-                <Tooltip label='Toggle Dark Mode'>
-                    <IconButton onClick={() => toggleColorMode()} fontSize='35px' colorScheme='yellow' variant='ghost' aria-label='Toggle Dark Mode' icon={<MoonIcon />}/>
-                </Tooltip>
+            <Tooltip label={colorMode === 'light' ? 'Toggle Dark Mode' : 'Toggle Light Mode'}>
+            <IconButton onClick={() => toggleColorMode()} fontSize='35px' colorScheme={colorMode === 'light' ? 'yellow' : 'yellow'} variant='ghost' aria-label='Toggle Dark Mode' icon={colorMode === 'light' ? <MdDarkMode />: <MdLightMode/> }/>
+        </Tooltip>
             </Flex>
             
         </div>
