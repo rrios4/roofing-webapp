965cafb39a97eb74627385126b7b7bd682a87728
Rogelio Rios <rogeliorios348@gmail.com>
2022-07-08 18:49:11 -0500

    z-index to Navbar to stay on top

--- client/src/components/Layout/Navbar/Navbar.jsx
+++ client/src/components/Layout/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import {MdDashboard, MdRequestPage, MdScheduleSend, MdSquareFoot, MdPeopleAlt} f
 
 const Navbar = () => {
     return (
-        <Flex position='fixed' w={{base: 'full', lg:'8rem'}} h={{base: '4rem', lg:'100vh'}} bg='gray.700' roundedTopRight={{base: '0', lg:'25'}} roundedBottomRight={{base: '0', lg:'25'}} flexDir={{base: 'row',lg: 'column'}} p='0' m='0' top={{base:'0', lg:'0'}}>
+        <Flex zIndex={'9999'} position='fixed' w={{base: 'full', lg:'8rem'}} h={{base: '4rem', lg:'100vh'}} bg='gray.700' roundedTopRight={{base: '0', lg:'25'}} roundedBottomRight={{base: '0', lg:'25'}} flexDir={{base: 'row',lg: 'column'}} p='0' m='0' top={{base:'0', lg:'0'}}>
             <Link to='/'>
                 <Box display='flex' flexDir={{base: 'column',lg:'column'}} h={{base: '4rem',lg:'8rem'}} bg='blue.400' shadow='xl' roundedRight='25' justifyContent={{base:'center', lg:'center'}} px={{base:'6px', lg:'0px'}}>
                     <Image marginLeft={{base: '-2px',lg:'12px'}} boxSize={{base: '50px',lg:'100px'}} src='https://github.com/rrios4/roofing-webapp/blob/main/client/src/assets/LogoRR.png?raw=true'/>
