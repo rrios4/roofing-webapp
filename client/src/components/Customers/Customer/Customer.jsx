a08eec03d8d19e766202e544fcac9d8355c9ddf5
Rogelio Rios <rogeliorios348@gmail.com>
2022-07-13 23:06:34 -0500

    Fixed customer type badge alignment

--- client/src/components/Customers/Customer/Customer.jsx
+++ client/src/components/Customers/Customer/Customer.jsx
@@ -12,9 +12,10 @@ const Customer = (props) => {
         {customers ? customers.map((customer,index) => (
             <Tr key={index}>
                 <Td textAlign={'center'}>{customer.first_name && customer.last_name ? <><Text marginRight={'2px'}>{customer.first_name}</Text><Text>{customer.last_name}</Text></> : <>{customer.company_name}</>}</Td>
-                <Td textAlign={'center'}>{customer.customer_type_id === 1 ? <><Box maxW={'100px'}><Text fontSize={'sm'} textAlign={'center'} p={'2'} rounded={'full'} bg={'blue.500'} textColor={'white'} fontWeight={'light'}>Residential</Text></Box></>  : ""}
-                    {customer.customer_type_id === 2 ? <><Box maxW={'100px'}><Text fontSize={'sm'}  textAlign={'center'} p={'2'} rounded={'full'} bg={'green.500'} textColor={'white'} fontWeight={'light'}>Commercial</Text></Box></> : ""}
-                    {customer.customer_type_id === 3 ? <><Box maxW={'100px'}><Text fontSize={'sm'}  textAlign={'center'} p={'2'} rounded={'full'} bg={'yellow.500'} textColor={'white'} fontWeight={'light'}>Other</Text></Box></>   : ""}  
+                <Td textAlign={'center'} isNumeric>
+                    {customer.customer_type_id === 1 ? <><Text fontSize={'sm'} maxW={'100px'} mx={'auto'} textAlign={'center'} p={'2'} rounded={'full'} bg={'blue.500'} textColor={'white'} fontWeight={'light'}>Residential</Text></>  : ""}
+                    {customer.customer_type_id === 2 ? <><Text fontSize={'sm'} maxW={'100px'} mx={'auto'} textAlign={'center'} p={'2'} rounded={'full'} bg={'green.500'} textColor={'white'} fontWeight={'light'}>Commercial</Text></> : ""}
+                    {customer.customer_type_id === 3 ? <><Text fontSize={'sm'} maxW={'100px'} mx={'auto'} textAlign={'center'} p={'2'} rounded={'full'} bg={'yellow.500'} textColor={'white'} fontWeight={'light'}>Other</Text></>   : ""}  
                 </Td>
                 <Td textAlign={'center'}>{customer.email}</Td>
                 <Td textAlign={'center'}>{customer.phone_number}</Td>
@@ -22,7 +23,7 @@ const Customer = (props) => {
                 <Td textAlign={'center'}>{new Date(customer.created_at).toLocaleDateString('en-us', options)}</Td>
                 <Td textAlign={'center'}><Link to={`/editcustomer/${customer.id}`}><Tooltip label='Go to Customer Details '><Button ml={'1rem'} colorScheme={'gray'} variant='solid'><MdKeyboardArrowRight size={'20px'}/></Button></Tooltip></Link></Td>
             </Tr>
-        )) : <><Td></Td><Td></Td><Td display={'flex'} justifyContent='center'><Spinner margin='1rem'/></Td> </>}
+        )) : <><Td></Td><Td></Td><Td></Td><Td></Td><Td display={'flex'} justifyContent='center'><Spinner margin='1rem'/></Td> </>}
     </>
   )
 }
