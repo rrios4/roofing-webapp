abc72f067576902dbb4cbfa78a44320a15548f4c
Rogelio Rios <rogeliorios348@gmail.com>
2023-05-22 15:42:39 -0500

    Cleaned up alert dialog for QR

--- src/components/ui/Alerts/ConnectedAlerts/ConnectedQRDeleteAlertDialog.jsx
+++ src/components/ui/Alerts/ConnectedAlerts/ConnectedQRDeleteAlertDialog.jsx
@@ -7,31 +7,6 @@ const ConnectedQRDeleteAlertDialog = (props) => {
   const { mutate, isLoading } = useDeleteQRById(toast);
 
   const handleSubmit = async () => {
-    // const { data, error } = await supabase.from('quote_request').delete().eq('id', itemNumber);
-
-    // if (error) {
-    //   toast({
-    //     position: `top`,
-    //     title: `Error occured deleting QR #${itemNumber}!`,
-    //     description: `Error: ${error.message}`,
-    //     status: 'error',
-    //     duration: 5000,
-    //     isClosable: true
-    //   });
-    // }
-
-    // if (data) {
-    //   queryClient.invalidateQueries({ queryKey: ['quoteRequests'] });
-
-    //   toast({
-    //     position: `top`,
-    //     title: `QR #${itemNumber} deleted!`,
-    //     description: `We've deleted QR #${itemNumber} for you succesfully! 🚀`,
-    //     status: 'success',
-    //     duration: 5000,
-    //     isClosable: true
-    //   });
-    // }
     mutate(itemNumber);
     // Closes Drawer
     onClose();
