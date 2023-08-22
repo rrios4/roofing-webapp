b4a5bda78b0bec2b110a1ff18cac9cc2ef5c6af0
Rogelio Rios <rogeliorios348@gmail.com>
2023-08-22 10:16:20 -0500

    Customer stat cards

--- src/pages/customers-page.tsx
+++ src/pages/customers-page.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import DefaultPageHeader from '../components/ui/page-header'
+import React from 'react';
+import DefaultPageHeader from '../components/ui/page-header';
+import {
+  useFetchTotalCustomers,
+  useFetchTotalResidentialCustomers,
+  useFetchTotalCommercialCustomers
+} from '../hooks/useAPI/useReports.jsx';
+import { BuildingIcon, StoreIcon, UserIcon } from 'lucide-react';
+import CountStatCard from '../components/count-stat-card';
 
-type Props = {}
+type Props = {};
 
 export default function CustomersPage({}: Props) {
+  const { data: totalCustomersCount, isLoading: isTotalCustomerCountLoading } =
+    useFetchTotalCustomers();
+  const { data: totalResidentialCustomers, isLoading: isTotalResidentialCustomersLoading } =
+    useFetchTotalResidentialCustomers();
+  const { data: totalCommercialCustomers, isLoading: isTotalCommercialCustomersLoading } =
+    useFetchTotalCommercialCustomers();
+  React.useEffect(() => {
+    console.log(totalCustomersCount);
+  }, []);
+
   return (
-    <div className='w-full'>
-      <DefaultPageHeader title='Customers' subheading='Manage customers and view information focused on them.' addItemTextButton='Add customer'/>
+    <div className="flex flex-col w-full gap-4">
+      <DefaultPageHeader
+        title="Customers"
+        subheading="Manage customers and view information focused on them."
+        addItemTextButton="Add customer"
+      />
+      <div className="flex w-full gap-4 flex-col md:flex-row">
+        <CountStatCard
+          title="Total Customers"
+          totalCount={totalCustomersCount}
+          icon={<UserIcon size={'25px'} />}
+          isLoading={isTotalCustomerCountLoading}
+        />
+        <CountStatCard
+          title="Residential"
+          totalCount={totalResidentialCustomers}
+          icon={<StoreIcon size={'25px'} />}
+          isLoading={isTotalResidentialCustomersLoading}
+        />
+        <CountStatCard
+          title="Commercial"
+          totalCount={totalCommercialCustomers}
+          icon={<BuildingIcon size={'25px'} />}
+          isLoading={isTotalCommercialCustomersLoading}
+        />
+      </div>
     </div>
-  )
+  );
 }
